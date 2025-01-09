d45ce061860a5a82a4f13db2a941e51b5ca599a0
Ramzan Niazi <ramzanniazi020@gmail.com>
2025-01-09 05:14:52 +0000

    1. Add new workflow action: -Change from Purchase Needed -Change Form Orderer Needed
    2. Edit function to accomodate different language in workflow action

--- comment_for_workflow_action/public/js/purchase_order.js
+++ comment_for_workflow_action/public/js/purchase_order.js
@@ -2,7 +2,7 @@ frappe.ui.form.on("Purchase Order", {
     before_workflow_action: async (frm) => {
         let promise = new Promise((resolve, reject) => {
             frappe.dom.unfreeze()
-            if (frm.selected_workflow_action == "Change Needed") {
+            if (frm.selected_workflow_action.trim().toLowerCase() == __("Change Needed").toLowerCase()) {
                 var d = new frappe.ui.Dialog({
                     title: __("What needs to be changed?"),
                     fields: [
@@ -14,7 +14,7 @@ frappe.ui.form.on("Purchase Order", {
                     ],
                     primary_action: function () {
                         var data = d.get_values();
-                        let change_needed = "What needs to be revised: " + data.change_needed;
+                        let change_needed = __("What needs to be revised: ") + data.change_needed;
     
                         frappe.call({
                             method: "frappe.desk.form.utils.add_comment",
@@ -33,7 +33,7 @@ frappe.ui.form.on("Purchase Order", {
                     },
                 });
                 d.show();
-            } else if (frm.selected_workflow_action == "Reject") {
+            } else if (frm.selected_workflow_action.trim().toLowerCase() == __("Reject").toLowerCase()) {
                 var d = new frappe.ui.Dialog({
                     title: __("Reason for Rejection"),
                     fields: [
@@ -45,7 +45,69 @@ frappe.ui.form.on("Purchase Order", {
                     ],
                     primary_action: function () {
                         var data = d.get_values();
-                        let reason_for_reject = "Reason for Rejection: " + data.reason_for_reject;
+                        let reason_for_reject = __("Reason for Rejection: ") + data.reason_for_reject;
+    
+                        frappe.call({
+                            method: "frappe.desk.form.utils.add_comment",
+                            args: {
+                                reference_doctype: frm.doctype,
+                                reference_name: frm.docname,
+                                content: __(reason_for_reject),
+                                comment_email: frappe.session.user,
+                                comment_by: frappe.session.user_fullname,
+                            },
+                            callback: function (r) {
+                                d.hide();
+                                resolve();
+                            },
+                        });
+                    },
+                });
+                d.show();
+            } else if (frm.selected_workflow_action.trim().toLowerCase() == __("Change from Purchase Needed").toLowerCase()) {
+                var d = new frappe.ui.Dialog({
+                    title: __("What needs to be changed?"),
+                    fields: [
+                        {
+                            fieldname: "reason_for_reject",
+                            fieldtype: "Text",
+                            reqd: 1,
+                        },
+                    ],
+                    primary_action: function () {
+                        var data = d.get_values();
+                        let reason_for_reject = __("What needs to be change:") + data.reason_for_reject;
+    
+                        frappe.call({
+                            method: "frappe.desk.form.utils.add_comment",
+                            args: {
+                                reference_doctype: frm.doctype,
+                                reference_name: frm.docname,
+                                content: __(reason_for_reject),
+                                comment_email: frappe.session.user,
+                                comment_by: frappe.session.user_fullname,
+                            },
+                            callback: function (r) {
+                                d.hide();
+                                resolve();
+                            },
+                        });
+                    },
+                });
+                d.show();
+            } else if (frm.selected_workflow_action.trim().toLowerCase() == __("Change Form Orderer Needed").toLowerCase()) {
+                var d = new frappe.ui.Dialog({
+                    title: __("What needs to be changed?"),
+                    fields: [
+                        {
+                            fieldname: "reason_for_reject",
+                            fieldtype: "Text",
+                            reqd: 1,
+                        },
+                    ],
+                    primary_action: function () {
+                        var data = d.get_values();
+                        let reason_for_reject = __("What needs to be change:") + data.reason_for_reject;
     
                         frappe.call({
                             method: "frappe.desk.form.utils.add_comment",
